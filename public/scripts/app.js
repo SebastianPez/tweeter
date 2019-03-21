cca07b61c8b474c2351e0aecb7f719f4730ef625
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-21 18:58:58 +0000

    Added renderTweets function to dynamically append tweets from DB, also removed hardcoded tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,22 +3,54 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
 
-const tweet = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+const tweets = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      "handle": "@SirIsaac"
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
-  }
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
 const createTweetElement = function (tweetData) {
     let $tweet = $('<article>').addClass('posted-tweets');
@@ -44,8 +76,18 @@ const createTweetElement = function (tweetData) {
     return $tweet;
 }
 
-$(document).ready(function() {
-    $('.tweet-container').append(createTweetElement(tweet));
+
+
+
+
+    const renderTweets = function (tweets) {
+        for (let i = 0; i < tweets.length; i ++) {
+            let newTweet = createTweetElement(tweets[i]);
+            $('section.tweet-container').append(newTweet);
+        }
+    }
+
+    renderTweets(tweets);
 
     $('main.container article.posted-tweets').hover(function () {
         $('.tweet-header').toggleClass('hover-changed');
